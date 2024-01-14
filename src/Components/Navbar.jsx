1cee51fd011e7fd6dfd52820feba11f6f53a81b4
Suhaas Teja <Suhaas@zestiot.io>
2024-01-14 23:18:06 +0530

    sidemenu added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,42 +1,40 @@
 import { useEffect, useState } from "react"
 import "./Navbar.css"
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+
 
 function Navbar() {
-    const [open, setOpen] = useState(false);
+    const [toggleMenu, setToggleMenu] = useState(false);
 
-    const handleWindowSize = (e) => {
-        const isSmallScreen = window.innerWidth <= 768;
-        setOpen(isSmallScreen);
+    const handleToggleMenu = e => {
+        setToggleMenu(prev => !prev);
     }
 
-    useEffect(() => {
-        window.addEventListener("resize", handleWindowSize);
 
-        return () => {
-            window.addEventListener("resize", handleWindowSize);
-
-        }
-        
-    }, []);
-
-
-  return (
-    <div className='nav-container flex'>
-        <div className="flex home">
-            <div className="dot"></div>
-            <Link to="/" className="name">Suhaas</Link>
+    return (
+        <div className='nav-container flex'>
+            <div className="flex home">
+                <div className="dot"></div>
+                <Link to="/" className="name">Suhaas</Link>
+            </div>
+            <Link to="/Work" className="item nav-item">Work</Link>
+            <Link to="/Projects" className="item nav-item">Projects</Link>
+            <Link to="/Skills" className="item nav-item">Skills</Link>
+            {/* <Link to="/Contact" className="item">Contact</Link> */}
+            <FontAwesomeIcon icon={faBars} onClick={handleToggleMenu} className="sidemenu-toggle" />
+            {
+                toggleMenu && <div className="sidebar-items">
+                    <FontAwesomeIcon icon={faXmark} onClick={handleToggleMenu} />
+                    <Link to="/Work" className="item sidebar-item" onClick={handleToggleMenu}>Work</Link>
+                    <Link to="/Projects" className="item sidebar-item" onClick={handleToggleMenu}>Projects</Link>
+                    <Link to="/Skills" className="item sidebar-item" onClick={handleToggleMenu}>Skills</Link>
+                </div>
+            }
         </div>
-          <Link to="/Work" className="item">Work</Link>
-          <Link to="/Projects" className="item">Projects</Link>
-          <Link to="/Skills" className="item">Skills</Link>
-          {/* <Link to="/Contact" className="item">Contact</Link> */}
-            
-
-
-
-    </div>
-  )
+    )
 }
 
 export default Navbar
