274d0ad8414f2e240f12429266db6903a30df1b5
Suhaas Teja <suhaas.teja@gmail.com>
2024-02-19 20:11:09 -0800

    added Kommunicate

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
+
+Kommunicate.init("171b0a7e8b34870c2310c049ded3b0efe", {
+  automaticChatOpenOnNavigation: true,
+  popupWidget: true
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
